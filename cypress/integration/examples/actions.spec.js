cdb82f448ea8ca68ae74e3d606ae9764d981ad9d
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-04-27 10:30:26 -0600

    Make form test Stub POST request

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -3,7 +3,11 @@
 context('Actions', () => {
   beforeEach(() => {
     cy
-    .intercept('http://localhost:3001/api/v1/orders', { fixture: 'orders'})
+    .intercept('GET','http://localhost:3001/api/v1/orders', { fixture: 'orders'})
+    .intercept('POST','http://localhost:3001/api/v1/order', {
+      statusCode: 200,
+      body: {id: 2, name: "Max B", ingredients: ["beans"]}
+    })
     .visit('http://localhost:3000')
   })
 
@@ -20,6 +24,21 @@ context('Actions', () => {
     .should('contain','carnitas')
     .should('contain', 'queso fresco')
     .should('contain', 'jalapeno')
+  })
+
+  it('Should have a form that can have a name input and ingredients selection that appears on page when the recipe is succefully subbmited to the server' , () => {
+    cy
+    .get('form').should('exist')
+    .get('input[name=name]').should('exist')
+    .should('have.attr', 'placeholder', 'Name')
+    .type('Max B')
+    .get('input[name=name]').should('have.attr', 'value', 'Max B')
+    .get('button').first().should('exist')
+    .click()
+    .get('.submit-button').should('exist')
+    .click()
+
+
   })
   
 })
